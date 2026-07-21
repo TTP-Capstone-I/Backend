b96485bae447f933ddef4854c4066203cf5402d6
Shun Lei Hlaing <shunlei@mac.bmcc.cuny.edu>
2026-07-21 14:34:11 -0400

    add a user model

---
+++ models/users.js
@@ -0,0 +1,21 @@
+const { DataTypes} = require('sequelize');
+const dbConnection = require('../db')
+
+const Users = dbConnection.define('user',{
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            notNull:{ msg: 'Email is required.'},
+            notEmpty:{ msg: 'Email is cannot be empty.'},
+            isEmail:{ msg: 'Must be a valid email.'}
+        }
+    }
+} );
+
+module.exports = Users
